b6f0455781bdbcd0ad3520c507390e9b62e50638
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2017-09-23 09:30:13 +0800

    js and some media queries
    added javascript for hamburger menu and played with some media queires to fix layouts.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//load the DOM
+$(document).ready(function(){
+
+  //log
+  console.log('ready!');
+
+  //click hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+      //show mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible')
+  });
+
+
+  //click x
+
+  //close mobile menu
+
+});
