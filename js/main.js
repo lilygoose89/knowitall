df44561b015389bbabd2b14bb76527fdd6987ff1
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2017-09-23 21:26:15 +0800

    fail at javascript
    here's hoping nothing else got fucked up in the process

--- js/main.js
+++ js/main.js
@@ -18,8 +18,44 @@ $(document).ready(function(){
 ** - jquery.flexslider.js
 */
 
-  // Send a message to console after window load
-  console.log('Window loaded!');
+  // Assign flexslider functoin to class .flexslider
+  $('.flexslider').flexslider({
+
+    // Customize flexslider properties
+    animation: "slide",
+    slideshowSpeed: 5000,
+    animationSpeed: 1000,
+    slideshow: false,
+    directionNav: false,
+    controlNav: false,
+    start: function (slider) {
+
+      // Click on the left arrow
+      $('#flex-left').on('click', function(event){
+
+        // prevent default action
+        event.preventDefault();
+
+        // Move to previous slide
+        $('.flexslider').flexslider('prev');
+
+      });
+
+      // Click on the right arrow
+      $('#flex-right').on('click', function(event){
+
+        // prevent default action
+        event.preventDefault();
+
+        // Move to next slide
+        $('.flexslider').flexslider('next');
+
+      });
+
+    }
+
+
+  });
 
   // Assign flexslider functoin to class .flexslider
   $('.flexslider').flexslider({
@@ -56,5 +92,6 @@ $(document).ready(function(){
       });
 
     }
+
 
   });
