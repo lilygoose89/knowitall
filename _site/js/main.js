be6ecb2efc525848b425f7677f964a7dd526e8ca
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2017-09-23 19:04:06 +0800

    js for slider
    added slider for testimonials

--- _site/js/main.js
+++ _site/js/main.js
@@ -11,9 +11,50 @@ $(document).ready(function(){
       $('#mobile-menu').toggleClass('mobile-menu-visible')
   });
 
-
-  //click x
-
-  //close mobile menu
-
 });
+/*
+** Includes:
+** - jquery.js
+** - jquery.flexslider.js
+*/
+
+  // Send a message to console after window load
+  console.log('Window loaded!');
+
+  // Assign flexslider functoin to class .flexslider
+  $('.flexslider').flexslider({
+
+    // Customize flexslider properties
+    animation: "slide",
+    slideshowSpeed: 5000,
+    animationSpeed: 1000,
+    slideshow: false,
+    directionNav: false,
+    controlNav: false,
+    start: function (slider) {
+
+      // Click on the left arrow
+      $('#flex-left').on('click', function(event){
+
+        // prevent default action
+        event.preventDefault();
+
+        // Move to previous slide
+        $('.flexslider').flexslider('prev');
+
+      });
+
+      // Click on the right arrow
+      $('#flex-right').on('click', function(event){
+
+        // prevent default action
+        event.preventDefault();
+
+        // Move to next slide
+        $('.flexslider').flexslider('next');
+
+      });
+
+    }
+
+  });
